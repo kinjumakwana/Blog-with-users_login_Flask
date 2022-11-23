d648ac1f186977f208c50f90ee24aa2d54733e71
kinjulmakwana <makwanakinjal5@gmail.com>
2022-11-23 18:24:32 +0530

    postgresql commit

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# when running locally
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 ckeditor = CKEditor(app)
